5eff20eac53d1270808283b527480c26f46df580
Minh <git6@gmail.com>
2023-12-01 07:42:22 +0700

    update news.js

--- _data/news.js
+++ _data/news.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 module.exports = async function() {
     try {
-      const response = await axios.get(`https://newsapi.org/v2/everything?q=tesla&from=2023-10-30&sortBy=publishedAt&apiKey=${process.env.API_KEY}&pageSize=5`);
+      const response = await axios.get(`https://newsapi.org/v2/everything?q=tesla&from=2023-11-01&sortBy=publishedAt&apiKey=${process.env.API_KEY}&pageSize=5`);
       return response.data;
     } catch (error) {
       console.error(error);
